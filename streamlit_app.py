03b9eec4213bc7d420282ee7474de2e204932f49
stuwils <149127101+stuwils@users.noreply.github.com>
2023-10-26 21:25:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Moms New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omege 3 & Blueberry Oatmeal')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')

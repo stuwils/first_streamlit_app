eb05e116de22c12d459bc8d6698d6b032cbdb53c
stuwils <149127101+stuwils@users.noreply.github.com>
2023-10-26 21:10:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.title('Breakfast Menu')
 ('Omege 3 & Blueberry Oatmeal')
+('Kale, Spinach & Rocket Smoothie')
+('Hard-Boiled Free-Range Egg')

245cace8f5251910790eed2ee6d5dee74e423425
stuwils <149127101+stuwils@users.noreply.github.com>
2023-10-26 21:13:12 -0500

    Update streamlit_app.py
    added streamlit.XXX syntax for header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title('Breakfast Menu')
-('Omege 3 & Blueberry Oatmeal')
-('Kale, Spinach & Rocket Smoothie')
-('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omege 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
